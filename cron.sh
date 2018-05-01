a3549a767c0a932cb57eab16d2e5548add10807d
Charles Severance <csev@umich.edu>
2018-05-01 17:43:57 -0400

    Add a random sleep between upgrade and update

--- cron.sh
+++ cron.sh
@@ -6,6 +6,9 @@ date
 # Install any needed tools if we are second to the cluster
 sudo su -s "/home/ubuntu/ami-sql/tool_update.sh" www-data
 
+# Wait some random time so we don't all hit at once
+sleep $[ ( $RANDOM % 60 ) + 1 ]s
+
 # Create/update the Tsugi database tables
 sudo su -s "/home/ubuntu/ami-sql/db_upgrade.sh" www-data
 
