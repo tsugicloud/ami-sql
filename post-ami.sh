83313846e60d123002add0d7746e91f285ca6f5d
Charles Severance <csev@umich.edu>
2018-08-27 00:18:07 -0400

    Add moar cron jobs

--- post-ami.sh
+++ post-ami.sh
@@ -170,9 +170,9 @@ echo ======= Cleanup Done
 # https://askubuntu.com/questions/2368/how-do-i-set-up-a-cron-job
 
 echo ====== Setting up cron jobs
-cp /home/ubuntu/ami-sql/cron.sh /home/ubuntu/cron.sh
-chown ubuntu:ubuntu /home/ubuntu/cron.sh
-chmod 664 /home/ubuntu/cron.sh
+cp /home/ubuntu/ami-sql/cron*.sh /home/ubuntu
+chown ubuntu:ubuntu /home/ubuntu/cron*.sh
+chmod 664 /home/ubuntu/cron*.sh
 
 cp /home/ubuntu/ami-sql/crontab.txt /var/spool/cron/crontabs/ubuntu
 chown ubuntu:ubuntu /var/spool/cron/crontabs/ubuntu
