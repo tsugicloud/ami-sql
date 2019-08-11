d5db0a28d9e57703064950c6a8e4eced7d4bfda2
Charles Severance <csev@umich.edu>
2019-08-11 11:29:29 -0400

    Attempt to move to 18.04 and PHP 7.2

--- pre-ami-7-1.sh
+++ pre-ami-7-1.sh
@@ -1,8 +1,6 @@
 # Setup a scalable app server instance behind ELB
 # Leave hostname alone
 
-# Ubuntu Server 18.04 LTS (HVM), SSD Volume Type - ami-05c1fa8df71875112
-
 export DEBIAN_FRONTEND=noninteractive
 export LC_ALL=C.UTF-8
 locale -a
@@ -22,7 +20,7 @@ echo ======= Update 2
 apt-get update
 add-apt-repository -y ppa:ondrej/php
 add-apt-repository -y ppa:ondrej/apache2
-# add-apt-repository -y ppa:ondrej/mysql-5.6
+add-apt-repository -y ppa:ondrej/mysql-5.6
 add-apt-repository -y ppa:certbot/certbot
 
 echo ======= Update 3
@@ -32,11 +30,13 @@ apt-get install -y apache2
 
 # apt-cache search php7
 
-apt-get install -y php7.3
-apt-get install -y libapache2-mod-php7.3 php7.3-mysql php7.3-curl php7.3-json
-apt-get install -y php7.3-mbstring php7.3-zip php7.3-xml php7.3-gd
-apt-get install -y php7.3-apc php7.3-intl
-apt-get install -y php-memcached php-memcache
+apt-get install -y php7.1
+apt-get install -y libapache2-mod-php7.1 php7.1-mysql php7.1-curl php7.1-json
+apt-get install -y php7.1-mbstring php7.1-zip php7.1-xml php7.1-gd
+apt-get install -y php7.1-apc
+apt-get install -y php7.1-intl
+apt-get install -y php-memcached
+apt-get install -y php-memcache
 
 
 apt-get install -y mysql-client
