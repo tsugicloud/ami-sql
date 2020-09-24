1edc3d432b66b08972d5913bd7904aa8f9e3f6af
Charles Severance <csev@umich.edu>
2020-09-23 23:43:51 -0400

    Add the -y

--- patch_ami.sh
+++ patch_ami.sh
@@ -1,7 +1,7 @@
 
 echo "Making sure we don't fill disk with old AMI bits"
 
-apt-get autoremove
-apt-get update
-apt-get autoremove
+apt-get -y autoremove
+apt-get -y update
+apt-get -y autoremove
 
