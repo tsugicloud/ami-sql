38e221f9eeb805a9360c0f0f7a984cf7f4ab05de
Charles Severance <csev@umich.edu>
2021-08-08 11:45:44 -0400

    Moar updates

--- pre-ami.sh
+++ pre-ami.sh
@@ -6,29 +6,32 @@ export LC_ALL=C.UTF-8
 locale -a
 env
 
+# from docker/base/tsugi-base-prepare.sh
+
 echo ======= Update 1
 apt update
-
-apt-get install -y build-essential python-software-properties software-properties-common
+echo ======= Upgrade
+apt upgrade
+apt-get install -y build-essential
+apt-get install -y software-properties-common
 apt-get install -y byobu curl git htop man unzip vim wget
+apt-get install -y apt-utils
+apt-get install -y mysql-client-8.0
+apt-get install -y nfs-common
+if [ ! -f "/usr/bin/crontab" ]; then
+    apt-get install -y cron
+fi
 apt-get install -y ca-certificates
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
-
 echo ======= Update 2
 apt update
 add-apt-repository -y ppa:ondrej/php
 add-apt-repository -y ppa:ondrej/apache2
+# https://certbot.eff.org/lets-encrypt/ubuntubionic-apache
+add-apt-repository -y universe
 add-apt-repository -y -r ppa:certbot/certbot
-
-echo ======= Update 3
 apt update
-
-# from docker/base/tsugi-base-prepare.sh
-
 apt-get install -y apache2
-apt-get install -y mysql-client-8.0
-apt-get install -y nfs-common  # For EFS
-
 apt-get install -y php8.0
 apt-get install -y libapache2-mod-php8.0 php8.0-mysql php8.0-curl
 apt-get install -y php8.0-mbstring php8.0-zip php8.0-xml php8.0-gd
@@ -38,6 +41,14 @@ apt-get install -y php8.0-memcached php8.0-memcache
 apt-get install -y certbot python-certbot-apache
 a2enmod -q rewrite dir expires headers
 phpenmod mysqlnd pdo_mysql intl
+
+echo ======= Installing Node and Friends
+apt-get install -y nodejs
+node --version
+apt-get install -y npm
+npm --version
+
+# End from docker/base/tsugi-base-prepare.sh
 
 echo ======= Cleanup Start
 df
